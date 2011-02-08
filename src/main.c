1445fa220afaef6ca31bbfcd42a261799b5ddbd6
sirius <sirius@6f01ea5f-641d-df11-ac65-003048919802>
2011-02-08 19:26:42 +0000

    git-svn-id: http://svn.openlgtv.org.ru/epk2extract/trunk@78 6f01ea5f-641d-df11-ac65-003048919802

--- src/main.c
+++ src/main.c
@@ -718,7 +718,7 @@ void extractRELEASE(const char *cramfs_image, const char *destination) {
 int main(int argc, char *argv[]) {
 
 	printf("LG electronics digital tv firmware EPK2 extractor\n");
-	printf("Version 0.6 by sirius (openlgtv.org.ru) 08/02/2011\n\n");
+	printf("Version 0.6 by sirius (openlgtv.org.ru) 08.02.2011\n\n");
 
 	SWU_CryptoInit();
 
