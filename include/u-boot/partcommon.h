ba021d92d6dc84b6b32a27cf602f31cd7c1dfe9a
smx <smx@smx.com>
2013-12-13 15:17:55 +0100

    Ok, now i added all missing files

---
+++ include/u-boot/partcommon.h
@@ -0,0 +1,36 @@
+#ifndef _PART_COMMON_H_
+#define _PART_COMMON_H_
+
+#define U64_UPPER(x)	(unsigned long)( x >> 32 )
+#define U64_LOWER(x)	(unsigned long)( x & 0xffffffff)
+
+/*#ifndef NO
+#define NO							0x00
+#define YES							0x01
+#endif*/
+
+/*-----------------------------------------------------------------------------
+ * partition info
+ */
+#define PART_FLG_FIXED				1
+#define PART_FLG_MASTER				2
+#define PART_FLG_IDKEY				4
+#define PART_FLG_CACHE				8
+#define PART_FLG_DATA					16
+#define PART_FLG_SECURED				32
+#define PART_FLG_ERASE				64
+
+#define STR_LEN_MAX					32
+
+#define PM_PARTITION_MAX			64
+
+typedef enum PART_INFO {
+	PART_INFO_IDX	= 0,
+	PART_INFO_OFFSET,
+	PART_INFO_SIZE,
+	PART_INFO_FILESIZE
+} PART_INFO_TYPE;
+
+extern int ps;
+
+#endif /* _PART_COMMON_H_ */
---
+++ include/u-boot/partinfov2.h
@@ -0,0 +1,46 @@
+#ifndef _PART_INFO2_H_
+#define _PART_INFO2_H_
+
+#define P2_PARTITION_MAX				128
+
+struct p2_device_info {
+	char			name[STR_LEN_MAX];
+	unsigned long long	size;
+	unsigned long long 	phys;
+	#if __x86_64__
+	unsigned int virt;
+	unsigned int cached;
+	#else
+	void			*virt;
+	void			*cached;
+	#endif
+	int				bankwidth;
+	unsigned int	used;
+};
+
+struct p2_partition_info {
+	char			name[STR_LEN_MAX];			/* identifier string                               */
+	unsigned long long	offset;					/* offset within the master MTD space              */
+	unsigned long long	size;					/* partition size                                  */
+	char			filename[STR_LEN_MAX];		/* file name                                       */
+	unsigned int	filesize;					/* file size                                       */
+	unsigned int	sw_ver;						/* software version                                */
+	unsigned char	used;						/* Is this partition is used?                      */
+	unsigned char	valid;						/* Is this partition is valid?                     */
+	unsigned int	mask_flags;					/* master MTD flags to mask out for this partition */
+};
+
+struct p2_partmap_info {
+	unsigned int					magic;
+	unsigned int					cur_epk_ver;
+	unsigned int					old_epk_ver;
+	unsigned char 					npartition;
+	struct p2_device_info			dev;
+	struct p2_partition_info		partition[P2_PARTITION_MAX];
+};
+
+#define P2_GET_PART_INFO(x)			((struct p2_partition_info *)&(p2_partinfo.partition[x]))
+
+extern struct p2_partmap_info		p2_partinfo;
+#endif /* _PART_INFO2_H_ */
+
