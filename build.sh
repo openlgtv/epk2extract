427b6aacd6316facf79943d38b3c055347c42150
Лёнчик <lprot@list.ru>
2014-01-06 23:12:51 +0200

    added deps for cygwin32:   build.sh

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ if [ ! "$1" == "clean" ]; then
 		elif [ "$rel" == "build_cygwin" ]; then
 			mv epk2extract.exe ../$rel
 			if [ "$HOSTTYPE" == "i686" ]; then #cygwin32
-				sharedlibs=("cygz.dll" "cygwin1.dll" "cyglzo2-2.dll" "cyggcc_s-1.dll" "cygcrypto-1.0.0.dll")
+				sharedlibs=("cygz.dll" "cygwin1.dll" "cyglzo2-2.dll" "cyggcc_s-1.dll" "cygcrypto-1.0.0.dll" "cygstdc++-6.dll")
 			elif [ "$HOSTTYPE" == "x86_64" ]; then #cygwin64
 				sharedlibs=("cygz.dll" "cygwin1.dll" "cyglzo2-2.dll" "cygcrypto-1.0.0.dll" "cyggcc_s-seh-1.dll" "cygstdc++-6.dll")
 			fi
