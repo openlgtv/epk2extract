f5eea755753bdab67a1d7ba793f7bb7f5a884827
smx <smx@smx.com>
2015-01-26 00:19:56 +0100

    Support indentation of single files or directories

--- code_format.sh
+++ code_format.sh
@@ -15,7 +15,18 @@ function my_indent() {
 			"$file"
 }
 
+if [ -z "$1" ]; then
+	dirs="src include"
+elif [ -d "$1" ]; then
+	dirs="$1"
+elif [ -f "$1" ]; then
+	echo "Reindenting ${1}..."
+	my_indent "$1"
+	exit 0
+fi
+
 for file in `find src include -type f -name "*.c" -or -name "*.cpp" -or -name "*.h"`; do
 	echo "Reindenting ${file}..."
 	my_indent "$file"
 done
+exit 0
