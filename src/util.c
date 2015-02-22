18c85f4f4fc969d236439e07d2efec39114531f5
smx <smx@smx.com>
2015-02-22 21:42:50 +0100

    util: include missing stdarg header

--- src/util.c
+++ src/util.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 #include <ctype.h>
 #include <ftw.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
