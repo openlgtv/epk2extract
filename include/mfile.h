dc46de62eef92e52027a1031965b35fe16731359
smx-smx <smx@smx.com>
2016-05-30 22:24:36 +0200

    Added MFILE for memory file operations

---
+++ include/mfile.h
@@ -0,0 +1,78 @@
+/*
+	A mmap file wrapper
+	Copyright 2016 Smx
+*/
+#ifndef __MFILE_H
+#define __MFILE_H
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#define MFILE_ANON(var, size) \
+	void *var = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, 0, 0)
+
+/* Gets the size of the memory mapped file */
+#define msize(mfile) mfile->statBuf.st_size
+/* Gets the data, casted to the type specified in the second argument */
+#define mdata(mfile, type) (type *)(mfile->pMem)
+/* Gets the file handler (for mfopen) */
+#define mfh(mfile) mfile->fh
+/* Gets the file offset */
+#define moff(mfile, ptr) (off_t)((uintptr_t)ptr - (uintptr_t)mfile->pMem)
+
+#define mwriteat(mfile, off, ptr, size) \
+	memcpy( \
+		(void *)(&(mdata(mfile, uint8_t))[off]), \
+		(void *)ptr, \
+		size \
+	)
+
+#define mwrite(ptr, size, nmemb, mfile) \
+	mwriteat( \
+		mfile, \
+		mfile->offset, \
+		ptr, \
+		(size * nmemb) \
+	); \
+	mfile->offset += (size * nmemb)
+	
+#define mrewind(mfile) mfile->offset = 0
+
+typedef struct {
+	uint8_t *ptr;
+	off_t offset;
+	size_t size;
+} cursor_t;
+
+typedef struct {
+	char *path;
+	int fd;
+	FILE *fh;
+	off_t offset;
+	size_t size;
+	int prot;
+	struct stat statBuf;
+	void *pMem;
+} MFILE;
+
+MFILE *mfile_new();
+
+void *mfile_map(MFILE *file, size_t size);
+void *mfile_map_private(MFILE *file, size_t size);
+
+MFILE *mopen(const char *path, int oflags);
+MFILE *mopen_private(const char *path, int oflags);
+
+MFILE *mfopen(const char *path, const char *mode);
+MFILE *mfopen_private(const char *path, const char *mode);
+
+int mgetc(MFILE *stream);
+int mputc(int c, MFILE *stream);
+
+int cgetc(cursor_t *stream);
+int cputc(int c, cursor_t *stream);
+
+int mclose(MFILE *mfile);
+#endif
---
+++ src/mfile.c
@@ -0,0 +1,207 @@
+/*
+	A mmap file wrapper
+	Copyright 2016 Smx
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include "mfile.h"
+
+#define PERMS_DEFAULT (mode_t)0666
+
+/*
+ * Creates a new mfile structure
+ */
+inline MFILE *mfile_new(){
+	MFILE *mem = calloc(1, sizeof(MFILE));
+	return mem;
+}
+
+/*
+ * Updates size and path to a file
+ */
+int _mfile_update_info(MFILE *file, const char *path){
+	if(path){
+		if(file->path)
+			free(file->path);
+		file->path = strdup(path);
+	}
+	if(stat(file->path, &(file->statBuf)) < 0)
+		return -1;
+	return 0;
+}
+
+/*
+ * Wrapper to mmap
+ */
+void *_mfile_map(MFILE *file, size_t mapSize, int mapFlags){
+	if(msize(file) < mapSize){
+		lseek(file->fd, mapSize-1, SEEK_SET);
+		uint8_t buf = 0x00;
+		write(file->fd, &buf, 1);
+		lseek(file->fd, 0, SEEK_SET);
+		_mfile_update_info(file, NULL);
+	}
+	if(file->pMem){
+
+	}
+	file->pMem = mmap(0, mapSize, file->prot, mapFlags, file->fd, 0);
+	if(file->pMem == MAP_FAILED){
+		//err_exit("mmap failed: %s\n", strerror(errno));
+		return NULL;
+	}
+	
+	return file->pMem;
+}
+
+inline void *mfile_map(MFILE *file, size_t mapSize){
+	return _mfile_map(file, mapSize, MAP_SHARED);
+}
+
+inline void *mfile_map_private(MFILE *file, size_t mapSize){
+	return _mfile_map(file, mapSize, MAP_PRIVATE);
+}
+
+/*
+ * Opens and maps a file with open
+ */
+MFILE *_mopen(const char *path, int oflags, int mapFlags){
+	MFILE *file = mfile_new();
+	file->fd = open(path, oflags, PERMS_DEFAULT);
+	if(file->fd < 0){
+		goto e0_ret;
+	}
+	
+	if(_mfile_update_info(file, path) < 0)
+		goto e1_ret;
+	
+	if((oflags & O_ACCMODE) == O_RDONLY) {
+		file->prot = PROT_READ;
+	} else if((oflags & O_ACCMODE) == O_WRONLY) {
+		file->prot = PROT_WRITE;
+	} else if((oflags & O_ACCMODE) == O_RDWR) {
+		file->prot = PROT_READ | PROT_WRITE;
+	}
+
+	size_t fileSz = msize(file);
+	if(fileSz > 0){
+		if(_mfile_map(file, fileSz, mapFlags) == MAP_FAILED){
+			goto e1_ret;
+		}
+	}
+	return file;
+
+	e1_ret:
+		close(file->fd);
+	e0_ret:
+		if(file->path)
+			free(file->path);
+		free(file);
+		return NULL;
+}
+
+inline MFILE *mopen(const char *path, int oflags){
+	return _mopen(path, oflags, MAP_SHARED);
+}
+
+inline MFILE *mopen_private(const char *path, int oflags){
+	return _mopen(path, oflags, MAP_PRIVATE);
+}
+
+
+int mgetc(MFILE *stream){
+	if(stream->offset > msize(stream))
+		return EOF;
+	return (unsigned int)(*(&((uint8_t *)(stream->pMem))[stream->offset++]));
+}
+int mputc(int c, MFILE *stream){
+	if(stream->offset > msize(stream))
+		return EOF;
+	((uint8_t *)(stream->pMem))[stream->offset] = (uint8_t)c;
+	stream->offset++;
+	return c;
+}
+
+
+int cgetc(cursor_t *stream){
+	if(stream->offset > stream->size)
+		return EOF;
+	return (unsigned int)(
+		*(&(
+			((unsigned char *)(stream->ptr))[stream->offset++]
+		))
+	);
+}
+int cputc(int c, cursor_t *stream){
+	if(stream->offset > stream->size)
+		return EOF;
+	((unsigned char *)(stream->ptr))[stream->offset++] = (unsigned char)c;
+	return c;
+}
+
+/*
+ * Closes an opened file and frees the structure
+ */
+int mclose(MFILE *mfile){
+	if(!mfile || mfile->fd < 0 || !mfile->pMem || mfile->size <= 0)
+		return -1;
+	if(munmap(mfile->pMem, mfile->size) < 0)
+		return -2;
+	free(mfile->path);
+	close(mfile->fd);
+	free(mfile);
+	mfile = NULL;
+	return 0;
+}
+
+/*
+ * Opens and maps a file with fopen
+ */
+MFILE *_mfopen(const char *path, const char *mode, int mapFlags){
+	MFILE *file = mfile_new();
+
+	file->fh = fopen(path, mode);
+	if(file->fh == NULL){
+		goto e0_ret;
+	}
+	file->fd = fileno(file->fh);
+	
+	if(_mfile_update_info(file, path) < 0)
+		goto e1_ret;
+
+	if(strstr(mode, "r") != NULL || strstr(mode, "+") != NULL){
+		file->prot |= PROT_READ;
+	}
+	if(strstr(mode, "w") != NULL){
+		file->prot |= PROT_WRITE;
+	}
+
+	size_t fileSz = msize(file);
+	if(fileSz > 0){
+		if(_mfile_map(file, fileSz, mapFlags) == MAP_FAILED){
+			goto e1_ret;
+		}
+	}
+
+	return file;
+
+	e1_ret:
+		fclose(file->fh);
+	e0_ret:
+		free(file);
+		return NULL;
+}
+
+inline MFILE *mfopen(const char *path, const char *mode){
+	return _mfopen(path, mode, MAP_SHARED);
+}
+
+inline MFILE *mfopen_private(const char *path, const char *mode){
+	return _mfopen(path, mode, MAP_PRIVATE);
+}
