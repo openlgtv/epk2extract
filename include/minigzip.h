033f60406d175a1c0f275518753d66650862965a
smx-smx <smx@smx.com>
2016-11-29 16:43:35 +0100

    Updated/Cleaned up minigzip

--- include/minigzip.h
+++ include/minigzip.h
@@ -1,79 +1,12 @@
 #ifndef MINIGZIP_H
-#    define	MINIGZIP_H
-#    include <stdio.h>
-#    include <zlib.h>
-
-#    define GZIP_CHUNK 0x4000
-#    define windowBits 15
-#    define ENABLE_ZLIB_GZIP 32
-
-#    define CALL_ZLIB(x) {                                                  \
-        int status;                                                     \
-        status = x;                                                     \
-        if (status < 0) {                                               \
-            fprintf (stderr,                                            \
-                     "%s:%d: %s returned a bad status of %d.\n",        \
-                     __FILE__, __LINE__, #x, status);                   \
-            exit (EXIT_FAILURE);                                        \
-        }                                                               \
-    }
-
-#    ifdef STDC
-#        include <string.h>
-#        include <stdlib.h>
-#    else
-extern void exit OF((int));
-#    endif
-
-#    ifdef USE_MMAP
-#        include <sys/types.h>
-#        include <sys/mman.h>
-#        include <sys/stat.h>
-#    endif
-
-#    if defined(MSDOS) || defined(OS2) || defined(WIN32)
-#        include <fcntl.h>
-#        include <io.h>
-#        define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
-#    else
-#        define SET_BINARY_MODE(file)
-#    endif
-
-#    ifdef VMS
-#        define unlink delete
-#        define GZ_SUFFIX "-gz"
-#    endif
-#    ifdef RISCOS
-#        define unlink remove
-#        define GZ_SUFFIX "-gz"
-#        define fileno(file) file->__file
-#    endif
-#    if defined(__MWERKS__) && __dest_os != __be_os && __dest_os != __win32_os
-#        include <unix.h>		/* for fileno */
-#    endif
-
-#    ifndef WIN32				/* unlink already in stdio.h for WIN32 */
-extern int unlink OF((const char *));
-#    endif
-
-#    ifndef GZ_SUFFIX
-#        define GZ_SUFFIX ".gz"
-#    endif
-#    define SUFFIX_LEN (sizeof(GZ_SUFFIX)-1)
-
-#    define BUFLEN      16384
-#    define MAX_NAME_LEN 1024
-
-#    ifdef MAXSEG_64K
-#        define local static
-   /* Needed for systems with limitation on stack size. */
-#    else
-#        define local
-#    endif
+#define MINIGZIP_H
+#include <zlib.h>
 
 void error(const char *msg);
 void gz_compress(FILE * in, gzFile out);
+#ifdef USE_MMAP
 int gz_compress_mmap(FILE * in, gzFile out);
+#endif
 void gz_uncompress(gzFile in, FILE * out);
 void file_compress(char *file, char *mode);
 void file_uncompress(char *infile, char *outfile);
