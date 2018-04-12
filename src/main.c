35aad41151bec9315a5ca4dfb24edb45b9416107
smx-smx <smxdev4@gmail.com>
2018-04-12 21:55:50 +0200

    Bumped version to 4.8 - cleaned usage and added myself as current maintainer

--- src/main.c
+++ src/main.c
@@ -179,9 +179,8 @@ int handle_file(char *file, config_opts_t *config_opts) {
 }
 
 int main(int argc, char *argv[]) {
-	printf("\nLG Electronics digital TV firmware package (EPK) extractor version 4.5 by sirius (http://openlgtv.org.ru)\n\n");
+	printf("\nLG Electronics digital TV firmware package (EPK) extractor version 4.8 (http://openlgtv.org.ru)\n\n");
 	if (argc < 2) {
-		printf("Thanks to xeros, tbage, jenya, Arno1, rtokarev, cronix, lprot, Smx and all other guys from openlgtv project for their kind assistance.\n\n");
 		printf("Usage: epk2extract [-options] FILENAME\n\n");
 		printf("Options:\n");
 		printf("  -c : extract to current directory instead of source file directory\n");
