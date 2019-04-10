openlgtv/epk2extract

commit 7f51874b43b8bf9e602a0f0c374454c332fb2e87

Fixed support for sym files without DWARF section

src/symfile.c
@@ -48,11 +48,11 @@ struct symfile_header {
 	uint32_t size;
 	uint32_t n_symbols;
 	uint32_t tail_size;
-} __attribute__ ((packed));
+}__attribute__((packed));
 
-struct sym_table sym_table = {.n_symbols = 0,.sym_entry = NULL,.hash = NULL,
-	.n_dwarf_lst = 0,.dwarf_lst = NULL,.dwarf_data = NULL,.sym_name = NULL
-};
+struct sym_table sym_table = { .n_symbols = 0, .sym_entry = NULL, .hash = NULL,
+		.n_dwarf_lst = 0, .dwarf_lst = NULL, .dwarf_data = NULL, .sym_name =
+				NULL };
 
 int symfile_load(const char *fname) {
 	int fd = -1;
@@ -90,13 +90,14 @@ int symfile_load(const char *fname) {
 	}
 
 	if ((header->size + sizeof(*header)) != (uint32_t) st_buf.st_size) {
-		say_error("bad file `%s' size: %su, expected size: %lu", fname, st_buf.st_size, header->size + sizeof(*header));
+		say_error("bad file `%s' size: %su, expected size: %lu", fname,
+				st_buf.st_size, header->size + sizeof(*header));
 
 		return -1;
 	}
 
 	if ((header->tail_size + sizeof(struct sym_entry) * header->n_symbols)
-		!= header->size) {
+			!= header->size) {
 		say_error("file `%s' is broken", fname);
 
 		return -1;
@@ -109,8 +110,7 @@ int symfile_load(const char *fname) {
 	has_hash = p;
 	p += sizeof(*has_hash);
 	if (*has_hash != 2 && *has_hash != 0) {
-		say_error("unsupported file `%s' format", fname);
-
+		say_error("unsupported file `%s' format (unexpected has_hash 0x%x)", fname, *has_hash);
 		return -1;
 	}
 
@@ -121,11 +121,6 @@ int symfile_load(const char *fname) {
 
 	has_dwarf = p;
 	p += sizeof(*has_dwarf);
-	if (*has_dwarf > 1) {
-		say_error("unsupported file `%s' format", fname);
-
-		return -1;
-	}
 
 	if (*has_dwarf == 1) {
 		sym_table.n_dwarf_lst = *(uint32_t *) p;
@@ -136,9 +131,10 @@ int symfile_load(const char *fname) {
 		p += sizeof(sym_table.dwarf_lst[0]) * sym_table.n_dwarf_lst;
 		sym_table.dwarf_data = p;
 		p += dwarf_data_size;
+		sym_table.sym_name = p;
+	} else {
+		sym_table.sym_name = (char *)has_dwarf;
 	}
-
-	sym_table.sym_name = p;
 
 	say_info("`%s' has been successfully loaded", fname);
 
@@ -148,7 +144,8 @@ int symfile_load(const char *fname) {
 uint32_t symfile_addr_by_name(const char *name) {
 	unsigned i = 0;
 	for (i = 0; i < sym_table.n_symbols; ++i) {
-		char *sym_name = sym_table.sym_name + sym_table.sym_entry[i].sym_name_off;
+		char *sym_name = sym_table.sym_name
+				+ sym_table.sym_entry[i].sym_name_off;
 
 		if (strcmp(sym_name, name) == 0)
 			return sym_table.sym_entry[i].addr;
@@ -170,19 +167,20 @@ void symfile_write_idc(const char *fname) {
 
 	unsigned i = 0;
 	for (i = 0; i < sym_table.n_symbols; ++i) {
-		char *sym_name = sym_table.sym_name + sym_table.sym_entry[i].sym_name_off;
+			char *sym_name = sym_table.sym_name
+					+ sym_table.sym_entry[i].sym_name_off;
 
-		uint32_t addr = sym_table.sym_entry[i].addr;
-		uint32_t end = sym_table.sym_entry[i].end;
+			uint32_t addr = sym_table.sym_entry[i].addr;
+			uint32_t end = sym_table.sym_entry[i].end;
 
-		//printf("%s: %x...%x\n", sym_name, addr, end);
+			//printf("%s: %x...%x\n", sym_name, addr, end);
 
-		fprintf(outfile, "MakeNameEx( 0x%x, \"%s\", SN_NOWARN | SN_CHECK);\n", addr, sym_name);
+			fprintf(outfile, "MakeNameEx( 0x%x, \"%s\", SN_NOWARN | SN_CHECK);\n", addr, sym_name);
 
-		fprintf(outfile, "if(SegName(0x%x)==\".text\") {\n", addr);
-		fprintf(outfile, "   MakeCode(0x%x);\n", addr);
-		fprintf(outfile, "   MakeFunction(0x%x, 0x%x);\n", addr, end);
-		fprintf(outfile, "};\n", addr);
+			fprintf(outfile, "if(SegName(0x%x)==\".text\") {\n", addr);
+			fprintf(outfile, "   MakeCode(0x%x);\n", addr);
+			fprintf(outfile, "   MakeFunction(0x%x, 0x%x);\n", addr, end);
+			fprintf(outfile, "};\n", addr);
 
 	}
 
@@ -198,10 +196,12 @@ void symfile_write_idc(const char *fname) {
 
 }
 
+
 const char *symfile_name_by_addr(uint32_t addr) {
 	int i = 0;
 	for (i = sym_table.n_symbols - 1; i >= 0; --i) {
-		if (sym_table.sym_entry[i].addr <= addr && sym_table.sym_entry[i].end > addr)
+		if (sym_table.sym_entry[i].addr <= addr && sym_table.sym_entry[i].end
+				> addr)
 			return sym_table.sym_name + sym_table.sym_entry[i].sym_name_off;
 	}
 

commit 38fe367e88428eae725435c5b3a310112cdf6665

symfile: Fix incorrect format string in symfile_write_idc
Warning from gcc:

src/symfile.c: In function ‘symfile_write_idc’:
src/symfile.c:184:4: warning: too many arguments for format [-Wformat-extra-args]
    fprintf(outfile, "};\n", addr);
    ^

Fixes: 7f51874 ("Fixed support for sym files without DWARF section")

src/symfile.c
@@ -181,7 +181,7 @@ void symfile_write_idc(const char *fname) {
 			fprintf(outfile, "if(SegName(0x%x)==\".text\") {\n", addr);
 			fprintf(outfile, "   MakeCode(0x%x);\n", addr);
 			fprintf(outfile, "   MakeFunction(0x%x, 0x%x);\n", addr, end);
-			fprintf(outfile, "};\n", addr);
+			fprintf(outfile, "};\n");
 
 	}
 