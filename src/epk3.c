openlgtv/epk2extract

commit ed81d33ba614b0902e5599adfde14a2062b1c35b

Added support for new epk3 flavour

src/epk3.c
@@ -15,10 +15,19 @@
 #include "util_crypto.h"
 
 /*
+ * Check for epk3 files with an additional heading signature (whole file signature?)
+ */
+int compare_epk3_new_header(uint8_t *header, size_t headerSize){
+	EPK_V3_HEADER_T *hdr = (EPK_V3_HEADER_T *)(header + SIGNATURE_SIZE);
+	return memcmp(hdr->epkMagic, EPK3_MAGIC, sizeof(hdr->epkMagic)) == 0;
+}
+
+/*
  * Checks if the given data is an EPK3 header
  */
 int compare_epk3_header(uint8_t *header, size_t headerSize){
 	EPK_V3_HEADER_T *hdr = (EPK_V3_HEADER_T *)header;
+
 	return memcmp(hdr->epkMagic, EPK3_MAGIC, sizeof(hdr->epkMagic)) == 0;
 }
 
@@ -30,17 +39,18 @@ MFILE *isFileEPK3(const char *epk_file) {
 	}
 
 	struct epk3_structure *epk3 = mdata(file, struct epk3_structure);
+	struct epk3_head_structure *head = &(epk3->head);
 
 	if(msize(file) < sizeof(*epk3)){
 		goto checkFail;
 	}
 	
 	// check if the epk magic is present (decrypted)
-	if(compare_epk3_header((uint8_t *)&(epk3->epkHeader), sizeof(EPK_V3_HEADER_T))){
+	if(compare_epk3_header((uint8_t *)&(head->epkHeader), sizeof(EPK_V3_HEADER_T))){
 		goto checkOk;
 	}
 
-	if(isEpkVersionString(epk3->platformVersion) && isEpkVersionString(epk3->sdkVersion))
+	if(isEpkVersionString(head->platformVersion) && isEpkVersionString(head->sdkVersion))
 		goto checkOk;
 
 
@@ -49,33 +59,60 @@ MFILE *isFileEPK3(const char *epk_file) {
 		return NULL;
 
 	checkOk:
-		printf("[EPK] Platform Version: %.*s\n", sizeof(epk3->platformVersion), epk3->platformVersion);
-		printf("[EPK] SDK Version: %.*s\n", sizeof(epk3->sdkVersion), epk3->sdkVersion);
+		printf("[EPK3] Platform Version: %.*s\n", sizeof(head->platformVersion), head->platformVersion);
+		printf("[EPK3] SDK Version: %.*s\n", sizeof(head->sdkVersion), head->sdkVersion);
 		return file;
 }
 
-void extractEPK3(MFILE *epk, config_opts_t *config_opts){
+void extractEPK3(MFILE *epk, FILE_TYPE_T epkType, config_opts_t *config_opts){
+	struct epk3_head_structure *head;
+	
 	struct epk3_structure *epk3 = mdata(epk, struct epk3_structure);
-	EPK_V3_HEADER_T *epkHeader = &(epk3->epkHeader);
-
+	struct epk3_new_structure *epk3_new = mdata(epk, struct epk3_new_structure);
 	{
+		uint8_t *data = mdata(epk, uint8_t);
+		switch(epkType){
+			case EPK_V3:
+				head = &((struct epk3_structure *)data)->head;
+				break;
+			case EPK_V3_NEW:
+				head = &((struct epk3_new_structure *)data)->head;
+				break;
+			default:
+				err_exit("Unsupported EPK3 variant\n");
+		}
+	}
+	
+	EPK_V3_HEADER_T *epkHeader = &(head->epkHeader);
+	if(config_opts->enableSignatureChecking)
+	{	
 		size_t signed_size = (
-			member_size(struct epk3_structure, epkHeader) +
-			member_size(struct epk3_structure, crc32Info) + 
-			member_size(struct epk3_structure, reserved)
+				sizeof(head->epkHeader) +
+				sizeof(head->crc32Info) +
+				sizeof(head->reserved)
 		);
-
+		
 		wrap_verifyimage(
-			epk3->signature,
+			head->signature,
 			epkHeader,
 			signed_size,
 			config_opts->config_dir
 		);
 	}
 
+	size_t headerSize;
+	switch(epkType){
+		case EPK_V3:
+			headerSize = sizeof(EPK_V3_HEADER_T);
+			break;
+		case EPK_V3_NEW:
+			headerSize = sizeof(EPK_V3_NEW_HEADER_T);
+			break;
+	}
+	
 	int result = wrap_decryptimage(
 		epkHeader,
-		sizeof(EPK_V3_HEADER_T),
+		headerSize,
 		epkHeader,
 		config_opts->dest_dir,
 		EPK_V3,
@@ -110,6 +147,23 @@ void extractEPK3(MFILE *epk, config_opts_t *config_opts){
 	printf("packageInfoSize: %d\n", epkHeader->packageInfoSize);
 	printf("bChunked: %d\n", epkHeader->bChunked);
 
+	if(epkType == EPK_V3_NEW){
+		EPK_V3_NEW_HEADER_T *extHeader = (EPK_V3_NEW_HEADER_T *)epkHeader;
+		if(extHeader->pakInfoMagic != PACKAGEINFO_MAGIC){
+			printf("Invalid packageInfo magic %04X\n", extHeader->pakInfoMagic);
+			return;
+		}
+		
+		printf("EncryptType: %.*s\n",
+			sizeof(extHeader->encryptType),
+			extHeader->encryptType
+		);
+		printf("UpdateType:  %.*s\n",
+			sizeof(extHeader->updateType),
+			extHeader->updateType
+		);
+	}
+	
 	char *fwVersion;
 	asprintf(&fwVersion, "%02x.%02x.%02x.%02x-%s",
 		epkHeader->epkVersion[3],
@@ -118,39 +172,74 @@ void extractEPK3(MFILE *epk, config_opts_t *config_opts){
 		epkHeader->epkVersion[0],
 		epkHeader->otaId
 	);
+	
 	sprintf(config_opts->dest_dir, "%s/%s", config_opts->dest_dir, fwVersion);
 	createFolder(config_opts->dest_dir);
 	free(fwVersion);
 
+	printf("---- begin ----\n");
+
+	PAK_V3_LISTHEADER_T *packageInfo;
+	PAK_V3_NEW_LISTHEADER_T *packageInfoNew;
+	
+	uint i = 0;
+	PAK_V3_HEADER_T *pak;
+
+	uintptr_t dataPtr;
+
+	switch(epkType){
+		case EPK_V3:
+			packageInfo = &(epk3->packageInfo);
+			pak = &(packageInfo->packages[i]);
+			dataPtr = (uintptr_t)packageInfo;
+			break;
+		case EPK_V3_NEW:
+			packageInfoNew = &(epk3_new->packageInfo);		
+			pak = &(packageInfoNew->packages[i]);
+			dataPtr = (uintptr_t)packageInfoNew;
+			break;
+	}
+	
 	/* Decrypt packageInfo */
 	result = wrap_decryptimage(
-		&(epk3->packageInfo),
+		dataPtr,
 		epkHeader->packageInfoSize,
-		&(epk3->packageInfo),
+		dataPtr,
 		config_opts->dest_dir,
 		RAW,
 		NULL
 	);
+	
 	if(result < 0){
 		return;
 	}
-
-	//hexdump(&(epk3->packageInfo), epkHeader->packageInfoSize);
-	PAK_V3_LISTHEADER_T *packageInfo = &(epk3->packageInfo);
+		
+	dataPtr += epkHeader->packageInfoSize;
 	
-	printf("---- begin ----\n");
-	uint i = 0;
-	PAK_V3_HEADER_T *pak = &(packageInfo->packages[i]);
+	int packageInfoCount;
 	
-	uintptr_t dataPtr = (uintptr_t)packageInfo + epkHeader->packageInfoSize;
+	switch(epkType){
+		case EPK_V3:
+			packageInfoCount = packageInfo->packageInfoCount;
+			break;
+		case EPK_V3_NEW:
+			packageInfoCount = packageInfoNew->packageInfoCount;
+			break;
+	}
+	
+	for(; i<packageInfoCount;){
+		if(pak->packageInfoSize != sizeof(*pak)){
+			printf("Warning: Unexpected packageInfoSize '%d', expected '%d'\n",
+					pak->packageInfoSize, sizeof(*pak)
+			);
+		}
 
-	for(; i<packageInfo->packageInfoCount;){
 		printf("\nPAK '%s' contains %d segment(s), size %d bytes:\n",
 			pak->packageName,
 			pak->segmentInfo.segmentCount,
 			pak->packageSize
 		);
-
+		
 		char *pakFileName;
 		asprintf(&pakFileName, "%s/%s.pak", config_opts->dest_dir, pak->packageName);
 
@@ -169,7 +258,11 @@ void extractEPK3(MFILE *epk, config_opts_t *config_opts){
 			segNo++, pak++, i++
 		){
 			dataPtr += SIGNATURE_SIZE; //skip segment signature
-
+		
+			if(epkType == EPK_V3_NEW){
+				dataPtr += SIGNATURE_SIZE;
+			}
+			
 			printf("  segment #%u (name='%s', version='%s', offset='0x%lx', size='%u bytes')\n",
 				segNo + 1,
 				pak->packageName,
@@ -186,12 +279,28 @@ void extractEPK3(MFILE *epk, config_opts_t *config_opts){
 				RAW,
 				NULL
 			);
+					
 			if(result < 0){
-				return;
+				return;	
 			}
-			mwrite(dataPtr, pak->segmentInfo.segmentSize, 1, pakFile);
+			
+			if(epkType == EPK_V3_NEW){
+				uint32_t decryptedSegmentIndex = *(uint32_t *)dataPtr;
+				if(decryptedSegmentIndex != i){
+					printf("Warning: Decrypted segment doesn't match expected index! (index: %d, expected: %d)\n",
+							decryptedSegmentIndex, i
+					);
+				}
+				mwrite(dataPtr + 4, pak->segmentInfo.segmentSize, 1, pakFile);
+			} else {
+				mwrite(dataPtr, pak->segmentInfo.segmentSize, 1, pakFile);
+			}
 
 			dataPtr += pak->segmentInfo.segmentSize;
+			
+			if(epkType == EPK_V3_NEW){
+				dataPtr += sizeof(uint32_t);
+			}
 		}
 
 		mclose(pakFile);

include/epk.h
@@ -22,6 +22,7 @@ typedef enum {
     EPK,
 	EPK_V2,
 	EPK_V3,
+	EPK_V3_NEW,
     PAK_V2,
     RAW
 } FILE_TYPE_T;

include/epk3.h
@@ -9,10 +9,12 @@
 #include "epk.h"
 
 #define EPK3_MAGIC "EPK3"
+#define PACKAGEINFO_MAGIC 0x52603554 //Little Endian
 
 int compare_epk3_header(uint8_t *header, size_t headerSize);
+int compare_epk3_new_header(uint8_t *header, size_t headerSize);
 MFILE *isFileEPK3(const char *epk_file);
-void extractEPK3(MFILE *epk, config_opts_t *config_opts);
+void extractEPK3(MFILE *epk, FILE_TYPE_T epkType, config_opts_t *config_opts);
 
 typedef struct __attribute__((packed)) {
 	char epkMagic[4];
@@ -21,6 +23,18 @@ typedef struct __attribute__((packed)) {
 	uint32_t packageInfoSize;
 	uint32_t bChunked;
 } EPK_V3_HEADER_T;
+
+typedef struct __attribute__((packed)) {
+	char epkMagic[4];
+	char epkVersion[4];
+	char otaId[32];
+	uint32_t packageInfoSize;
+	uint32_t bChunked;
+	uint32_t pakInfoMagic;
+	uint8_t encryptType[6];
+	uint8_t updateType[3];
+	uint8_t reserved[1399];
+} EPK_V3_NEW_HEADER_T;
 
 typedef struct __attribute__((packed)) {
 	// SegmentInfo
@@ -57,15 +71,42 @@ typedef struct __attribute__((packed)) {
 	PAK_V3_HEADER_T packages[];
 } PAK_V3_LISTHEADER_T;
 
-struct  __attribute__((packed)) epk3_structure {
+typedef struct __attribute__((packed)) {
+	uint32_t packageInfoListSize;
+	uint32_t packageInfoCount;
+	uint32_t pakInfoMagic;
+	PAK_V3_HEADER_T packages[];
+} PAK_V3_NEW_LISTHEADER_T;
+
+struct __attribute__((packed)) epk3_head_structure {
 	signature_t signature;
 	EPK_V3_HEADER_T epkHeader;
 	uint32_t crc32Info[384];
 	uint32_t reserved; //or unknown
 	char platformVersion[16];
 	char sdkVersion[16];
+};
+
+struct __attribute__((packed)) epk3_new_head_structure {
+	signature_t signature;
+	EPK_V3_NEW_HEADER_T epkHeader;
+	char platformVersion[16];
+	char sdkVersion[16];
+};
+
+struct  __attribute__((packed)) epk3_structure {
+	struct epk3_head_structure head;
     signature_t packageInfo_signature;
 	PAK_V3_LISTHEADER_T packageInfo;
+};
+
+struct __attribute__((packed)) epk3_new_structure {
+	signature_t signature;
+	struct epk3_new_head_structure head;
+
+	signature_t packageInfo_signature;
+	signature_t sig2;
+	PAK_V3_NEW_LISTHEADER_T packageInfo;
 };
 
 struct __attribute__((packed)) pak3_structure {

include/mfile.h
@@ -21,7 +21,7 @@ extern "C" {
 /* Gets the size of the memory mapped file */
 #define msize(mfile) mfile->statBuf.st_size
 /* Gets the data, casted to the type specified in the second argument */
-#define mdata(mfile, type) (type *)(mfile->pMem)
+#define mdata(mfile, type) ((type *)(mfile->pMem))
 /* Gets the file handler (for mfopen) */
 #define mfh(mfile) mfile->fh
 /* Gets the file offset */

src/epk.c
@@ -36,6 +36,8 @@ int compare_epak_header(uint8_t *header, size_t headerSize){
 		return EPK_V2;
 	} else if(compare_epk3_header(header, headerSize)){
 		return EPK_V3;
+	} else if(compare_epk3_new_header(header, headerSize)){
+		return EPK_V3_NEW;
 	}
 
 	return 0;
@@ -291,8 +293,9 @@ void extractEPKfile(const char *epk_file, config_opts_t *config_opts){
 				extractEPK2(epk, config_opts);
 				break;
 			case EPK_V3:
+			case EPK_V3_NEW:
 				printf("[+] EPK v3 Detected\n");
-				extractEPK3(epk, config_opts);
+				extractEPK3(epk, epkType, config_opts);
 				break;
 		}
 	} while(0);

src/epk2.c
@@ -65,8 +65,8 @@ MFILE *isFileEPK2(const char *epk_file) {
 
 	checkOk:
 		if(has_versions){
-			printf("[EPK] Platform Version: %.*s\n", sizeof(epk2->platformVersion), epk2->platformVersion);
-			printf("[EPK] SDK Version: %.*s\n", sizeof(epk2->sdkVersion), epk2->sdkVersion);
+			printf("[EPK2] Platform Version: %.*s\n", sizeof(epk2->platformVersion), epk2->platformVersion);
+			printf("[EPK2] SDK Version: %.*s\n", sizeof(epk2->sdkVersion), epk2->sdkVersion);
 		}
 		return file;
 

commit a4d4172cd856460395e3747d9061b0ee8eb8ba95

epk3: Cast unsigned integer type uintptr_t to 'void *'
C Standard states this type is "an unsigned integer type with the property that any valid pointer
to void can be converted to this type, then converted back to pointer to void, and the result
will compare equal to the original pointer"

Whilst it says nothing about actual size, it does need to be cast back to pointer to void if
it's being passed to a function parameter 'void *'. This conversion is not automatic.

Compiler warning with clang 7.0.0:

src/epk3.c:200:3: warning: incompatible integer to pointer conversion passing 'uintptr_t'
      (aka 'unsigned long') to parameter of type 'void *' [-Wint-conversion]
                dataPtr,
                ^~~~~~~
include/epk.h:32:29: note: passing argument to parameter 'src' here
int wrap_decryptimage(void *src, size_t datalen, void *dest, char *confi...
                            ^
src/epk3.c:202:3: warning: incompatible integer to pointer conversion passing 'uintptr_t'
      (aka 'unsigned long') to parameter of type 'void *' [-Wint-conversion]
                dataPtr,
                ^~~~~~~
include/epk.h:32:56: note: passing argument to parameter 'dest' here
int wrap_decryptimage(void *src, size_t datalen, void *dest, char *confi...
                                                       ^

Fixes: ed81d33 ("Added support for new epk3 flavour")

src/epk3.c
@@ -197,9 +197,9 @@ void extractEPK3(MFILE *epk, FILE_TYPE_T epkType, config_opts_t *config_opts){
 	
 	/* Decrypt packageInfo */
 	result = wrap_decryptimage(
-		dataPtr,
+		(void *)dataPtr,
 		epkHeader->packageInfoSize,
-		dataPtr,
+		(void *)dataPtr,
 		config_opts->dest_dir,
 		RAW,
 		NULL