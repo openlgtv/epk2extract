c09be8923d77ff03fae9708ded3c95466e1ca0ae
Stefano Moioli <smxdev4@gmail.com>
2020-03-01 21:08:24 +0100

    Travis CI: added macOS environment

---
+++ .travis/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+install_linux(){
+	sudo apt-get update -qq
+	sudo apt-get install -qq liblzo2-dev libssl-dev libc6-dev
+}
+
+install_osx(){
+	brew update
+	brew install zlib lzo openssl@1.1
+}
+
+case $TRAVIS_OS_NAME in
+	osx) install_osx;;
+	linux) install_linux;;
+	*)
+		echo "Unsupported OS ${TRAVIS_OS_NAME}"
+		exit 1
+		;;
+esac
