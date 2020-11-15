58359c4b0353602159651706ae80d97c8bfbcc8d
Stefano Moioli <smxdev4@gmail.com>
2020-11-15 18:40:56 +0100

    tsfile.c: refactoring - Use MFILE for the input file - Split the code in different functions

--- src/stream/tsfile.c
+++ src/stream/tsfile.c
@@ -1,3 +1,21 @@
+/**
+ * TS file Decryption
+ * (C) 2020 the epk2extract authors
+ **/
+
+/* Transport Stream Header (or 4-byte prefix) consists of 32-bit:
+	Sync byte						8bit	0x47
+	Transport Error Indicator (TEI)	1bit	Set by demodulator if can't correct errors in the stream, to tell the demultiplexer that the packet
+											has an uncorrectable error [11]
+	Payload Unit Start Indicator	1bit	1 means start of PES data or PSI otherwise zero only.
+	Transport Priority				1bit	1 means higher priority than other packets with the same PID.
+	PID								13bit	Packet ID
+	Scrambling control				2bit	'00' = Not scrambled. The following per DVB spec:[12]   
+											'01' = Reserved for future use, '10' = Scrambled with even key, '11' = Scrambled with odd key
+	Adaptation field exist			2bit	'01' = no adaptation fields, payload only, '10' = adaptation field only, '11' = adaptation field and payload
+	Continuity counter				4bit	Incremented only when a payload is present (i.e., adaptation field exist is 01 or 11)[13]
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -113,193 +131,249 @@ static int setKey(char *keyPath) {
 	return 0;
 }
 
+// minimum number of TS packets (sync bytes) that must be present
+#define MIN_TS_PACKETS 3
+
+uint8_t *findTsPacket(MFILE *tsFile, long offset){
+	if(offset >= msize(tsFile)){
+		return NULL;
+	}
+
+	uint8_t *head;
+	uint8_t *cur;
+	
+	int syncPackets;
+	
+	do {
+		syncPackets = 0;
+
+		// find initial sync position sequentially
+		for(head = mdata(tsFile, uint8_t) + offset;
+			moff(tsFile, head) < msize(tsFile) && *head != 0x47;
+			head++
+		);
+
+		// EOF condition
+		if(*head != 0x47) break;
+
+		++syncPackets;
+		cur = head;
+
+		// found the initial sync. now check next packets
+		for(int i=0;
+			//
+			syncPackets<MIN_TS_PACKETS
+			&& moff (tsFile, cur) < msize(tsFile)
+			&& cur[TS_PACKET_SIZE * i] == 0x47;
+			//
+			syncPackets++,
+			cur+=TS_PACKET_SIZE,
+			i++);
+
+	} while(syncPackets<MIN_TS_PACKETS);
+
+	if(syncPackets < MIN_TS_PACKETS){
+		return NULL;
+	}
+
+	if((head - 4) < mdata(tsFile, uint8_t)){
+		// corrupted file, we went before the file's beginning
+		return NULL;
+	}
+
+	// go to the beginning of the "Transport Stream Header"
+	return head - 4;
+}
+
+void writeHeaders(FILE *outFile){
+	uint8_t outBuf[TS_PACKET_SIZE];
+
+	{ // Construct and write PAT
+		memset(outBuf, 0xFF, TS_PACKET_SIZE);
+		uint8_t PAT[] = {
+			0x47, 0x40, 0x00, 0x10, 0x00, 0x00, 0xB0, 0x0D,
+			0x00, 0x06, 0xC7, 0x00, 0x00, 0x00, 0x01, 0xE0,
+			0xB1, 0xA2, 0x89, 0x69, 0x78
+		};
+		memcpy(outBuf, &PAT, sizeof(PAT));
+		fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
+	}
+
+	{ // Write empty PMT
+		memset(outBuf, 0xFF, TS_PACKET_SIZE);
+		fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
+	}
+}
+
+
+struct tables {
+	int number[8192];
+	unsigned char type[8192];
+	int pcr_count[8192];
+};
+
+void writePMT(struct tables *PIDs, FILE *outFile){
+	// Gather information for PMT construction
+	unsigned char stream_count = 0;
+	for (unsigned int i = 0; i < 8192; i++) {
+		//Count video stream PIDs (0xE0-0xEF)
+		if (PIDs->type[i] >= 0xE0 && PIDs->type[i] <= 0xEF) {
+			stream_count++;
+		}
+		//Count audio stream PIDs (0xC0-0xDF)
+		else if (PIDs->type[i] >= 0xC0 && PIDs->type[i] <= 0xDF) {
+			stream_count++;
+		}
+	}
+	unsigned char PMT_size = 21 + stream_count * 5;
+
+	uint8_t outBuf[TS_PACKET_SIZE];
+	// Fill PMT
+	memset(outBuf, 0xFF, TS_PACKET_SIZE);
+	
+	uint8_t PMT[TS_PACKET_SIZE - 4];
+	memset(PMT, 0xFF, TS_PACKET_SIZE - 4);
+
+	const uint8_t PMT_header[17] = {
+		0x47, 0x40, 0xB1, 0x10, 0x00, 0x02, 0xB0,
+		0x00,				// section length in bytes including crc
+		0x00, 0x01,			// program number
+		0xC1, 0x00, 0x00,
+		0xE4, 0x7E,			// PCR PID
+		0xF0, 0x00			// Program info length
+	};
+	memcpy(PMT, PMT_header, sizeof(PMT_header));
+	PMT[7] = 13 + stream_count * 5;
+
+	stream_count = 0;
+	for (unsigned int i = 0; i < 8192; i++)
+		if (PIDs->number[i] > 0) {
+			//printf("PID %zX : %d Type: %zX PCRs: %zX\n", i, PIDs.number[i], PIDs.type[i], PIDs.pcr_count[i]);
+			if (PIDs->pcr_count[i] > 0) {	// Set PCR PID
+				PMT[13] = ((i >> 8) & 0xFF) + 0xE0;
+				PMT[14] = i & 0xFF;
+			}
+			//Set video stream data in PMT
+			if (PIDs->type[i] >= 0xE0 && PIDs->type[i] <= 0xEF) {
+				PMT[17 + stream_count * 5] = 0x1B; 						// stream type ITU_T_H264
+				PMT[18 + stream_count * 5] = ((i >> 8) & 0xFF) + 0xE0;	// PID
+				PMT[19 + stream_count * 5] = i & 0xFF;
+				PMT[20 + stream_count * 5] = 0xF0;						// ES info length
+				PMT[21 + stream_count * 5] = 0x00;
+				stream_count++;
+			}
+			//Set audio stream data in PMT
+			else if (PIDs->type[i] >= 0xC0 && PIDs->type[i] <= 0xDF) {
+				PMT[17 + stream_count * 5] = 0x04; 						// stream type ISO/IEC 13818-3 Audio (MPEG-2)
+				PMT[18 + stream_count * 5] = ((i >> 8) & 0xFF) + 0xE0;	//PID
+				PMT[19 + stream_count * 5] = i & 0xFF;
+				PMT[20 + stream_count * 5] = 0xF0;						// ES info length
+				PMT[21 + stream_count * 5] = 0x00;
+				stream_count++;
+			}
+		}
+	// Set CRC32
+	uint32_t crc = str_crc32(&PMT[5], PMT[7] - 1);
+	PMT[PMT_size - 4] = (crc >> 24) & 0xFF;
+	PMT[PMT_size - 3] = (crc >> 16) & 0xFF;
+	PMT[PMT_size - 2] = (crc >> 8) & 0xFF;
+	PMT[PMT_size - 1] = crc & 0xFF;
+	memcpy(outBuf, PMT, sizeof(PMT));
+	
+	fseek(outFile, 0xBC, SEEK_SET);
+	fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
+}
+
+void processTsPacket(uint8_t *packet, struct tables *PIDs, FILE *outFile){
+	uint8_t outBuf[TS_PACKET_SIZE];
+
+	int offset = 8;
+	if ((packet[7] & 0xC0) == 0xC0 || (packet[7] & 0xC0) == 0x80) {	// decrypt only scrambled packets
+		// packet is encrypted, so we copy the original (readonly) for modifications
+		memcpy(outBuf, packet, TS_PACKET_SIZE);
+		// now set the pointer to the copy
+		packet = &outBuf[0];
+
+		if (packet[7] & 0x20){
+			offset += (packet[8] + 1);	// skip adaption field
+		}
+		packet[7] &= 0x3F;	// remove scrambling bits
+		if (offset > TS_PACKET_SIZE){
+			//application will crash without this check when file is corrupted
+			offset = TS_PACKET_SIZE;
+		}
+		unsigned int rounds = (TS_PACKET_SIZE - offset) / AES_BLOCK_SIZE;
+		for (unsigned int k = 0; k < rounds; k++){
+			// in-place decrypt
+			AES_decrypt(
+				&packet[offset + k * AES_BLOCK_SIZE],
+				&packet[offset + k * AES_BLOCK_SIZE],
+				&AESkey);	// AES CBC
+		}
+	};
+
+	// Search PCR
+	if (packet[7] & 0x20) {	// adaptation field exists
+		if (packet[9] & 0x10)	// check if PCR exists
+			PIDs->pcr_count[(packet[5] << 8 | packet[6]) & 0x1FFF]++;
+	}
+	// Count PES packets only
+	if (packet[8] == 0 && packet[9] == 0 && packet[10] == 1) {
+		PIDs->number[(packet[5] << 8 | packet[6]) & 0x1FFF]++;
+		PIDs->type[(packet[5] << 8 | packet[6]) & 0x1FFF] = packet[11];
+	}
+	fwrite(outBuf + 4, 1, TS_PACKET_SIZE - 4, outFile);
+}
+
 void convertSTR2TS_internal(char *inFilename, char *outFilename, int notOverwrite) {
-	FILE *inFile = fopen(inFilename, "rb");
+	MFILE *inFile = mopen(inFilename, O_RDONLY);
 	if (inFile == NULL) {
 		printf("Can't open file %s\n", inFilename);
 		return;
 	}
 
-	fseeko(inFile, 0, SEEK_END);
-	uint64_t filesize = ftello(inFile);
-	rewind(inFile);
-
-	FILE *outFile;
-	if (notOverwrite)
-		outFile = fopen(outFilename, "a+b");
-	else
-		outFile = fopen(outFilename, "wb");
-
-	if (outFile == NULL) {
-		printf("Can't open file %s\n", outFilename);
-		return;
-	}
-
-	unsigned char inBuf[TS_PACKET_SIZE * 10];
-	unsigned char outBuf[TS_PACKET_SIZE];
-	unsigned int k, rounds;
-	int syncFound = 0, j;
-	fread(inBuf, 1, sizeof(inBuf), inFile);
-
-	uint64_t i;
-	struct tables {
-		int number[8192];
-		unsigned char type[8192];
-		int pcr_count[8192];
-	};
-
 	struct tables PIDs;
 	memset(&PIDs, 0, sizeof(PIDs));
-	int PATnotWritten = 0;
-	for (i = 0; i < (sizeof(inBuf) - TS_PACKET_SIZE * 2); i++) {
-		if (inBuf[i] == 0x47 && inBuf[i + TS_PACKET_SIZE] == 0x47 && inBuf[i + TS_PACKET_SIZE * 2] == 0x47) {
-			fseeko(inFile, i - 4, SEEK_SET);
-			for (i = 0; i < filesize; i += TS_PACKET_SIZE) {
-				fread(inBuf, 1, TS_PACKET_SIZE, inFile);
-				if (inBuf[4] != 0x47) {
-					printf("\nLost sync at offset %" PRIx64 "\n", i);
-					fseeko(inFile, i, SEEK_SET);
-					syncFound = 0;
-					while (syncFound == 0) {
-						if (fread(inBuf, 1, sizeof(inBuf), inFile) < sizeof(inBuf))
-							break;	//prevent infinite loop at end of file
-						for (j = 0; j < (sizeof(inBuf) - TS_PACKET_SIZE); j++) {
-							if (inBuf[j] == 0x47 && inBuf[j + TS_PACKET_SIZE] == 0x47 && inBuf[j + TS_PACKET_SIZE * 2] == 0x47) {
-								syncFound = 1;
-								fseeko(inFile, i + j, SEEK_SET);
-								break;
-							}
-						}
-						i += sizeof(inBuf);
-					}
-				} else {
-					if (!notOverwrite && !PATnotWritten) {
-						// Construct PAT
-						memset(outBuf, 0xFF, TS_PACKET_SIZE);
-						unsigned char PAT[21] = { 0x47, 0x40, 0x00, 0x10, 0x00, 0x00, 0xB0, 0x0D, 0x00, 0x06, 0xC7, 0x00, 0x00, 0x00, 0x01,
-							0xE0, 0xB1, 0xA2, 0x89, 0x69, 0x78
-						};
-						memcpy(outBuf, &PAT, sizeof(PAT));
-						fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
 
-						// Allocate PMT
-						memset(outBuf, 0xFF, TS_PACKET_SIZE);
-						fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
-						PATnotWritten = 1;
-					}
-					memcpy(outBuf, inBuf, TS_PACKET_SIZE);
-					int offset = 8;
-					if ((inBuf[7] & 0xC0) == 0xC0 || (inBuf[7] & 0xC0) == 0x80) {	// decrypt only scrambled packets
-						if (inBuf[7] & 0x20)
-							offset += (inBuf[8] + 1);	// skip adaption field
-						outBuf[7] &= 0x3F;	// remove scrambling bits
-						if (offset > TS_PACKET_SIZE)
-							offset = TS_PACKET_SIZE;	//application will crash without this check when file is corrupted
-						rounds = (TS_PACKET_SIZE - offset) / 0x10;
-						for (k = 0; k < rounds; k++)
-							AES_decrypt(inBuf + offset + k * 0x10, outBuf + offset + k * 0x10, &AESkey);	// AES CBC
-					};
+	do {
+		FILE *outFile;
+		if (notOverwrite){
+			outFile = fopen(outFilename, "a+b");
+		} else {
+			outFile = fopen(outFilename, "wb");
+		}
 
-					// Search PCR
-					if (inBuf[7] & 0x20) {	// adaptation field exists
-						if (outBuf[9] & 0x10)	// check if PCR exists
-							PIDs.pcr_count[(inBuf[5] << 8 | inBuf[6]) & 0x1FFF]++;
-					}
-					// Count PES packets only
-					if (!notOverwrite && outBuf[8] == 0 && outBuf[9] == 0 && outBuf[10] == 1) {
-						PIDs.number[(inBuf[5] << 8 | inBuf[6]) & 0x1FFF]++;
-						PIDs.type[(inBuf[5] << 8 | inBuf[6]) & 0x1FFF] = outBuf[11];
-					}
-					fwrite(outBuf + 4, 1, TS_PACKET_SIZE - 4, outFile);
-				}
-			}
+		if (outFile == NULL) {
+			fprintf(stderr, "Can't open file %s\n", outFilename);
 			break;
 		}
-	}
-	fclose(inFile);
-	if (!notOverwrite) {
 
-		// Gather information for PMT construction
-		unsigned char stream_count = 0;
-		for (i = 0; i < 8192; i++) {
-			//Count video stream PIDs (0xE0-0xEF)
-			if (PIDs.type[i] >= 0xE0 && PIDs.type[i] <= 0xEF) {
-				stream_count++;
+		do {
+			uint8_t *packet = findTsPacket(inFile, 0);
+			if(packet == NULL){
+				fprintf(stderr, "Could not find sync\n");
+				break;
 			}
-			//Count audio stream PIDs (0xC0-0xDF)
-			else if (PIDs.type[i] >= 0xC0 && PIDs.type[i] <= 0xDF) {
-				stream_count++;
+
+			writeHeaders(outFile);
+
+			long offset;
+			for(;(offset=moff(inFile, packet)) < msize(inFile);
+				packet += TS_PACKET_SIZE
+			){
+				if(packet[4] != 0x47){
+					fprintf(stderr, "\nLost sync at offset %" PRIx64 "\n", offset);
+					packet = findTsPacket(inFile, offset);
+				}
+				processTsPacket(packet, &PIDs, outFile);
 			}
-		}
-		unsigned char PMT_size = 21 + stream_count * 5;
+		} while(0);
 
-		// Fill PMT
-		memset(outBuf, 0xFF, TS_PACKET_SIZE);
-		unsigned char PMT[TS_PACKET_SIZE - 4];
-		memset(PMT, 0xFF, TS_PACKET_SIZE - 4);
-
-		static const unsigned char PMT_header[17] = { 0x47, 0x40, 0xB1, 0x10, 0x00, 0x02, 0xB0,
-			0x00,				// section length in bytes including crc
-			0x00, 0x01,			// program number
-			0xC1, 0x00, 0x00,
-			0xE4, 0x7E,			// PCR PID
-			0xF0, 0x00			// Program info length
-		};
-		memcpy(PMT, PMT_header, sizeof(PMT_header));
-		PMT[7] = 13 + stream_count * 5;
-
-		stream_count = 0;
-		for (i = 0; i < 8192; i++)
-			if (PIDs.number[i] > 0) {
-				//printf("PID %zX : %d Type: %zX PCRs: %zX\n", i, PIDs.number[i], PIDs.type[i], PIDs.pcr_count[i]);
-				if (PIDs.pcr_count[i] > 0) {	// Set PCR PID
-					PMT[13] = ((i >> 8) & 0xFF) + 0xE0;
-					PMT[14] = i & 0xFF;
-				}
-				//Set video stream data in PMT
-				if (PIDs.type[i] >= 0xE0 && PIDs.type[i] <= 0xEF) {
-					PMT[17 + stream_count * 5] = 0x1B; 						// stream type ITU_T_H264
-					PMT[18 + stream_count * 5] = ((i >> 8) & 0xFF) + 0xE0;	// PID
-					PMT[19 + stream_count * 5] = i & 0xFF;
-					PMT[20 + stream_count * 5] = 0xF0;						// ES info length
-					PMT[21 + stream_count * 5] = 0x00;
-					stream_count++;
-				}
-				//Set audio stream data in PMT
-				else if (PIDs.type[i] >= 0xC0 && PIDs.type[i] <= 0xDF) {
-					PMT[17 + stream_count * 5] = 0x04; 						// stream type ISO/IEC 13818-3 Audio (MPEG-2)
-					PMT[18 + stream_count * 5] = ((i >> 8) & 0xFF) + 0xE0;	//PID
-					PMT[19 + stream_count * 5] = i & 0xFF;
-					PMT[20 + stream_count * 5] = 0xF0;						// ES info length
-					PMT[21 + stream_count * 5] = 0x00;
-					stream_count++;
-				}
-			}
-		// Set CRC32
-		uint32_t crc = str_crc32(&PMT[5], PMT[7] - 1);
-		PMT[PMT_size - 4] = (crc >> 24) & 0xFF;
-		PMT[PMT_size - 3] = (crc >> 16) & 0xFF;
-		PMT[PMT_size - 2] = (crc >> 8) & 0xFF;
-		PMT[PMT_size - 1] = crc & 0xFF;
-		memcpy(outBuf, PMT, sizeof(PMT));
-		fseek(outFile, 0xBC, SEEK_SET);
-		fwrite(outBuf, 1, TS_PACKET_SIZE - 4, outFile);
-	}
-	fclose(outFile);
+		writePMT(&PIDs, outFile);
+		fclose(outFile);
+	} while(0);
+	mclose(inFile);
 }
-
-/* Transport Stream Header (or 4-byte prefix) consists of 32-bit:
-	Sync byte						8bit	0x47
-	Transport Error Indicator (TEI)	1bit	Set by demodulator if can't correct errors in the stream, to tell the demultiplexer that the packet
-											has an uncorrectable error [11]
-	Payload Unit Start Indicator	1bit	1 means start of PES data or PSI otherwise zero only.
-	Transport Priority				1bit	1 means higher priority than other packets with the same PID.
-	PID								13bit	Packet ID
-	Scrambling control				2bit	'00' = Not scrambled. The following per DVB spec:[12]   
-											'01' = Reserved for future use, '10' = Scrambled with even key, '11' = Scrambled with odd key
-	Adaptation field exist			2bit	'01' = no adaptation fields, payload only, '10' = adaptation field only, '11' = adaptation field and payload
-	Continuity counter				4bit	Incremented only when a payload is present (i.e., adaptation field exist is 01 or 11)[13]
-*/
 
 void convertSTR2TS(char *inFilename, int notOverwrite) {
 	char *baseDir = my_dirname(inFilename);
@@ -340,7 +414,11 @@ void processPIF(const char *filename, char *dest_file) {
 	
 	char *keyPath;	
 	asprintf(&keyPath, "%s/dvr", baseDir);
-	setKey(keyPath);
+
+	if (0 != setKey(keyPath)){
+		free(keyPath);
+		err_exit("Load DVR Key-file failed for %s/dvr\n", baseDir);
+	}
 	free(keyPath);
 	
 	int append = 0;
