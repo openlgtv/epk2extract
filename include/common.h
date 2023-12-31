2865cd34cba824ccd2760398799d207f6dd64bb0
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2023-12-31 13:06:12 -0500

    common.h: __builtin_expect is a GCC extension

--- include/common.h
+++ include/common.h
@@ -6,8 +6,13 @@
 #define __COMMON_H
 
 /* Branch Prediction Hints */
+#ifdef __GNUC__
 #define LIKELY(x)    __builtin_expect (!!(x), 1)
 #define UNLIKELY(x)  __builtin_expect (!!(x), 0)
+#else
+#define LIKELY(x)	(x)
+#define UNLIKELY(x)	(x)
+#endif
 
 #ifdef __GNUC__
 #define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
