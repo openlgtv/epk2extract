ea06bda78580cff96d0e1dd2b0bd1d2d4f7328c3
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-01-10 19:38:17 -0500

    epk.c: fix bug in signature verification
    EVP_VerifyFinal() returns -1 on error, and therefore so did
    API_SWU_VerifyImage(). The check in wrap_SWU_VerifyImage() would treat
    a return value of -1 as success, meaning it could treat an invalid
    signature as having been successfully verified.

--- src/epk.c
+++ src/epk.c
@@ -89,7 +89,7 @@ static EVP_PKEY *SWU_CryptoInit_PEM(const char *configuration_dir, const char *p
 /*
  * Verifies the signature of the given data against the loaded public key
  */
-static int API_SWU_VerifyImage(const void *signature, const void *data, size_t imageSize, SIG_TYPE_T sigType) {
+static bool API_SWU_VerifyImage(const void *signature, const void *data, size_t imageSize, SIG_TYPE_T sigType) {
 	size_t hashSize;
 	unsigned int sigSize;
 	const EVP_MD *algo;
@@ -107,7 +107,7 @@ static int API_SWU_VerifyImage(const void *signature, const void *data, size_t i
 			break;
 		default:
 			printf("Invalid sigType: %d\n", sigType);
-			return 0;
+			return false;
 	}
 
 	unsigned char md_value[hashSize];
@@ -116,11 +116,11 @@ static int API_SWU_VerifyImage(const void *signature, const void *data, size_t i
 
 	EVP_MD_CTX *ctx1, *ctx2;
 	if ((ctx1 = EVP_MD_CTX_new()) == NULL)
-		return 0;
+		return false;
 
 	if ((ctx2 = EVP_MD_CTX_new()) == NULL) {
 		EVP_MD_CTX_free(ctx1);
-		return 0;
+		return false;
 	}
 
 	EVP_DigestInit(ctx1, algo);
@@ -134,7 +134,7 @@ static int API_SWU_VerifyImage(const void *signature, const void *data, size_t i
 	EVP_MD_CTX_free(ctx2);
 	EVP_MD_CTX_free(ctx1);
 
-	return result;
+	return (result == 1);
 }
 
 /*
@@ -145,7 +145,7 @@ static int wrap_SWU_VerifyImage(
 	size_t signedSize, size_t *effectiveSignedSize, SIG_TYPE_T sigType
 ){
 	size_t curSize = signedSize;
-	int verified;
+	bool verified;
 	//int skipped = 0;
 	while (curSize > 0) {
 		verified = API_SWU_VerifyImage(signature, data, curSize, sigType);
